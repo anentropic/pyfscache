4fd5daabea14d2b79615f10240e59d23da57bd90
Anentropic <ego@anentropic.com>
2015-01-29 10:31:53 +0000

    remove use of 3rd party lib in setup.py, so pip install can work

--- setup.py
+++ setup.py
@@ -2,27 +2,31 @@
 
 import os
 
+from ConfigParser import ConfigParser
 from setuptools import setup, find_packages
 
-import configobj
 
-info = configobj.ConfigObj('PackageInfo.cfg')
+config = ConfigParser()
+config.read('PackageInfo.cfg')
+info = dict(config.items('main'))
 
-setup(name = info['PACKAGE'],
-      version = "%(MAJOR)s.%(MINOR)s.%(MICRO)s%(TAG)s" % info,
-      author = info['AUTHOR'],
-      author_email = info['EMAIL'],
-      url = info['URL'],
-      description = info['DESCRIPTION'],
-      license = info['LICENSE'],
-      long_description = open('README.rst').read(),
-      packages = find_packages(),
-      package_data = {'':[os.path.join('*', '*.*')]},
-      include_package_data = True,
-      # requires = [],
-      test_suite = info['PACKAGE'] + '.tests.test_suite',
-      classifiers = [
-            'Programming Language :: Python :: 2.5',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7', ],
-      )
+setup(
+    name=info['package'],
+    version="%(major)s.%(minor)s.%(micro)s%(tag)s" % info,
+    author=info['author'],
+    author_email=info['email'],
+    url=info['url'],
+    description=info['description'],
+    license=info['license'],
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    package_data={'': [os.path.join('*', '*.*')]},
+    include_package_data=True,
+    # requires=[],
+    test_suite=info['package'] + '.tests.test_suite',
+    classifiers=[
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
